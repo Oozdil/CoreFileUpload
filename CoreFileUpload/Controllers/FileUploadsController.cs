f79e48fc0170e0f6051ceede24166b4ad5d23a7c
orçun özdil <eczorcun@hotmail.com>
2022-02-05 16:57:42 +0300

    Add project files.

---
+++ CoreFileUpload/Controllers/FileUploadsController.cs
@@ -0,0 +1,55 @@
+﻿using CoreFileUpload.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreFileUpload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadsController : ControllerBase
+    {
+        public static IWebHostEnvironment _webHostEnvironment;
+
+        public FileUploadsController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        [HttpPost]
+        public string Post([FromForm]FileUpload objectFile)
+        {
+            try
+            {
+                if(objectFile.files.Length>0)
+                {
+                    string path = _webHostEnvironment.WebRootPath + "\\uploads\\";
+                    if(!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    using (FileStream fileStream=System.IO.File.Create(path+objectFile.files.FileName))
+                    {
+                        objectFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "File Uploaded";
+
+                    }
+
+                }
+                else
+                {
+                    return "Not Uploaded";
+                }
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
